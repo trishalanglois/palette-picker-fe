02e7428e35ce8680c9b33cc19406d72efd408f82
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-09 15:44:26 -0700

    Update action type ADD_PROJECT

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -2,6 +2,8 @@ export const projects = (state = [], action) => {
     switch (action.type) {
     case 'ADD_PROJECTS':
         return [...state, ...action.projects]
+    case 'ADD_PROJECT':
+        return [...state, action.project]
     default:
         return state
     }
