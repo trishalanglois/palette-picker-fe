23a58aa3c88de7171f7d8d54476fdfd49ed1ab7c
“Foster <fosterataylor@yahoo.com>
2020-02-08 19:26:58 -0700

    Edit spaces in projects reducer

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -1,9 +1,8 @@
 export const projects = (state = [], action) => {
-   switch (action.type) {
-       case 'ADD_PROJECTS':
-           return [...state, ...action.projects]
-   
-       default:
-           return state
+    switch (action.type) {
+    case 'ADD_PROJECTS':
+        return [...state, ...action.projects]
+    default:
+        return state
    }
 }
