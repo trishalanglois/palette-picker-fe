c2ad3183bb876b13458f0fdba45f87aebafa17e6
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-08 18:37:35 -0700

    Add styline to project title input and generate palette button

--- src/Components/ProjectForm/ProjectForm.js
+++ src/Components/ProjectForm/ProjectForm.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './ProjectForm.scss';
 
-export const ProjectForm = () => {
+export const ProjectForm = (props) => {
+  const projectTitle = props.valueOf();
+
+
+
+
+
   return (
-    <> 
-      this is the project form!
-    </>
+    <div className="project-form-wrapper"> 
+      <form className="project-name-form">
+        {/* <h2 className="project-name-main"></h2> */}
+
+          <label className="title-input-label">PROJECT TITLE</label>
+          <input className="title-input" type="text" /> 
+
+        <button className="generate-palette-button" type="button">generate palette</button>
+      </form>
+
+    </div>
   )
 }
 
+export const mapStateToProps = state => ({
+
+})
+
 export default ProjectForm;
+
+// regular props, passed down from clickevent from sidebar - show the project title 
+// mapDispatchToProps - create new project, rerender App so it shows up in the sidebar, also shows up on the main card 
