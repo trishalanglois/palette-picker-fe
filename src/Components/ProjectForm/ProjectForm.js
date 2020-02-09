e7314c73445966a91dedb7d249e4943b22e2e53d
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-09 13:16:47 -0700

    Remove commented code and add useSelector to ProjectForm component

--- src/Components/ProjectForm/ProjectForm.js
+++ src/Components/ProjectForm/ProjectForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useSelector } from 'react';
 import { useDispatch } from 'react-redux';
 import './ProjectForm.scss';
 import { postProject } from '../../util/apiCalls';
@@ -8,31 +8,19 @@ export const ProjectForm = (props) => {
   const [title, setTitle] = useState("");
   const dispatch = useDispatch();
 
-
-  //re-render to show title (mapState) -- useEvent but listens for the generate palette button click?
-
-  useEffect(() => {
-    //mapState to set title = what's in store 
-  }, [dispatch])
-
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const response = await postProject(title)
-      // const project = await response.json();
       dispatch(addProject(response.id, title))
     } catch (error) {
       console.log(error)
     }
-    //generate 5 random colors and render paletteForm component 
   }
 
   return (
     <div className="project-form-wrapper"> 
       <form className="project-name-form">
-        {/* <h2 className="project-name-main"></h2> */}
-
           <label className="title-input-label">PROJECT TITLE</label>
           <input className="title-input" type="text" onChange={(event) => setTitle(event.target.value)}/> 
 
