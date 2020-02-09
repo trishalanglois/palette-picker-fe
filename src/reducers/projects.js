c36b7084766e742ed7a1c14cf31b9596e64cc506
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-09 13:33:57 -0700

    Update connecting ProjectForm to redux

--- src/reducers/projects.js
+++ src/reducers/projects.js
@@ -2,7 +2,7 @@ export const projects = (state = [], action) => {
     switch (action.type) {
     case 'ADD_PROJECTS':
         return [...state, ...action.projects]
-    case 'ADD_PROEJCT':
+    case 'ADD_PROJECT':
         return [...state, action.project]
     default:
         return state
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { projects } from './projects';
-import { project } from './project';
 
 const rootReducer = combineReducers({
     projects,
-    projects: project
 })
 
 export default rootReducer;
