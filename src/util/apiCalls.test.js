ff4ff7eea7c7efe33ad9cdf4fef35cee100714d7
“Foster <fosterataylor@yahoo.com>
2020-02-10 12:32:20 -0700

    Add retrievePalletes test to apiCallsTest

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -1,4 +1,4 @@
-import { retrieveProjects } from './apiCalls';
+import { retrieveProjects, retrievePalettes } from './apiCalls';
 
 
 
@@ -103,3 +103,68 @@ describe('retrieveProjects', () => {
     expect(retrieveProjects()).rejects.toEqual(Error('Error fetching projects'))
   })
 })
+
+describe('retrievePalettes', () => {
+    let mockResponse = [{
+        id: 20,
+        name: 'trish colors',
+        color1: '#F7C59F',
+        color2: '#2A324B',
+        color3: '#767B91',
+        color4: '#C7CCDB',
+        color5: '#E1E5EE',
+        project_id: 1
+    },
+    {
+        id: 21,
+        name: 'foster colors',
+        color1: '#57739',
+        color2: '#BDD5EA',
+        color3: '#F7F7FF',
+        color4: '#FE5F55',
+        color5: '#495867',
+        project_id: 1
+
+    },
+    {
+        id: 23,
+        name:'heather colors',
+        color1:'#000000',
+        color2:'#363946',
+        color3:'#696773',
+        color4:'#819595',
+        color5:'#B1B6A6',
+        project_id: 1
+}]
+
+  
+  
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockResponse)
+        });
+      });
+    });
+  
+    it('should be passed the correct url', () => {
+        let id = 1;
+      retrievePalettes(id)
+  
+      expect(window.fetch).toHaveBeenCalledWith(`https://mysterious-dusk-17585.herokuapp.com/api/v1/projects/${id}/palettes`);
+    })
+  
+    it('should return an array of projects', () => {
+      expect(retrievePalettes()).resolves.toEqual(mockResponse);
+    })
+  
+    it('should return an error for response that is not ok', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: false
+        });
+      });
+      expect(retrievePalettes()).rejects.toEqual(Error('Error fetching palettess'))
+    })
+  })
