cc6a020b9ca2d7c2c5a9d8423a6bdafd5f132ee6
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-09 19:54:46 -0700

    Add hooks to SidebarProject triggered by onClick

--- src/Components/SidebarProject/SidebarProject.js
+++ src/Components/SidebarProject/SidebarProject.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SidebarProject.scss';
 
-export const SidebarProject = ({ title }) => {
+export const SidebarProject = ({ title, id }) => {
+
+  const [clickedProject, setClickedProject] = useState('');
+
+  // const handleChange = event => {
+  //   setClickedProject(event.target.id);
+  //   console.log(event.target.id);
+  // }
+
+  //onClick - setClickedProject to the event.target.value 
+  //when clickedProject changes, useEffect will fire 
+  //returns new <ProjectForm component?
+
   return (
     <> 
-      <h2>{title}</h2>
+      <button id={id} onClick={(event) => setClickedProject(event.target.id)}>{title}</button>
     </>
   )
 }
 
 export default SidebarProject;
+
+
+//change h2s to buttons, interpolate their value/id? -- which also needs to be passed down from Sidebar 
+
+//when a user clicks, create <ProjectForm> with the title of the project passed as props
+//styling -- when it's been clicked, keep an active state on the title 
