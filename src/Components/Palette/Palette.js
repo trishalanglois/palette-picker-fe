12d7f1722a5e66de66549f3c237a874aee29dccf
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-11 14:40:52 -0700

    Transfer rendering random colors from Palette to PaletteForm

--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -2,30 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './Palette.scss';
 import Color from '../Color/Color';
 
-export const Palette = () => {
-  let newColors;
-  const [currentColors, setCurrentColors] = useState({
-    color1: {id: 1, color: ''},
-    color2: {id: 2, color: ''},
-    color3: {id: 3, color: ''},
-    color4: {id: 4, color: ''},
-    color5: {id: 5, color: ''}
-  })
+export const Palette = ({ currentColors }) => {
 
-
-  const generatePalette = () => {
-    setCurrentColors({
-      color1: {id: 1, color: Math.floor(Math.random()*16777215).toString(16)},
-      color2: {id: 2, color: Math.floor(Math.random()*16777215).toString(16)},
-      color3: {id: 3, color: Math.floor(Math.random()*16777215).toString(16)},
-      color4: {id: 4, color: Math.floor(Math.random()*16777215).toString(16)},
-      color5: {id: 5, color: Math.floor(Math.random()*16777215).toString(16)}
-    })
-  }
-
-  useEffect(() => {
-    generatePalette();
-  }, [])
 
   const renderPalette = () => {
     const colorKeys = Object.keys(currentColors);
@@ -43,7 +21,6 @@ export const Palette = () => {
 
   return (
     <aside className="palette-wrapper">
-      <button className="delete-button">X</button>
       <div className="palettes-wrapper">
         {renderPalette()}
       </div>
--- src/Components/PaletteForm/PaletteForm.js
+++ src/Components/PaletteForm/PaletteForm.js
@@ -5,30 +5,45 @@ import PalettesContainer from '../PalettesContainer/PalettesContainer';
 import Palette from '../Palette/Palette';
 
 export const PaletteForm = () => {
+  const [currentColors, setCurrentColors] = useState({
+    color1: {id: 1, color: ''},
+    color2: {id: 2, color: ''},
+    color3: {id: 3, color: ''},
+    color4: {id: 4, color: ''},
+    color5: {id: 5, color: ''}
+  })
+
+  const generatePalette = () => {
+    setCurrentColors({
+      color1: {id: 1, color: Math.floor(Math.random()*16777215).toString(16)},
+      color2: {id: 2, color: Math.floor(Math.random()*16777215).toString(16)},
+      color3: {id: 3, color: Math.floor(Math.random()*16777215).toString(16)},
+      color4: {id: 4, color: Math.floor(Math.random()*16777215).toString(16)},
+      color5: {id: 5, color: Math.floor(Math.random()*16777215).toString(16)}
+    })
+  }
+
+  useEffect(() => {
+    generatePalette();
+  }, [])
 
   return (
     <form className="add-palette-form">
-
-
       <div className="add-palette-form-top">
         <div className="name-palette-wrapper">
           <label className="name-palette-label">PALETTE NAME</label>
           <input className="name-palette-input" type="text"></input>
         </div>
         <div className="palettes-wrapper">
-          <Palette />
+          <Palette 
+            currentColors={currentColors} />
         </div>
       </div>
+      <button className="generate-palette-button" type="button">new palette</button>
       <button className="add-palette-button" type="button">add to project</button>
-
-      //if there are palettes to go with this project, render PalettesContainer: 
-      <PalettesContainer />
+      {/* <PalettesContainer /> */}
     </form>
   )
 }
 
 export default PaletteForm;
-
-
-//props will hold 5 random colors from ProjectForm's generate colors 
-//add to project button will fire POST request, re-render new PaletteForm
