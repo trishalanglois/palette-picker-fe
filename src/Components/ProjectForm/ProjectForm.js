514425887e9117141497f6317dc59092dce02483
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-11 13:58:01 -0700

    Attempt to update master

--- src/Components/ProjectForm/ProjectForm.js
+++ src/Components/ProjectForm/ProjectForm.js
@@ -1,34 +1,36 @@
-import React, { useState, useEffect, useSelector } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './ProjectForm.scss';
 import { postProject } from '../../util/apiCalls';
 import { addProject } from '../../actions/actions';
-
-export const ProjectForm = (props) => {
+export const ProjectForm = ({ projectTitle, id }) => {
   const [title, setTitle] = useState("");
   const dispatch = useDispatch();
-
+  const clickedProject = useSelector(state => state.clickedProject)
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const response = await postProject(title)
       dispatch(addProject(response.id[0], title))
     } catch (error) {
-      console.log(error)
     }
   }
-
   return (
     <div className="project-form-wrapper"> 
       <form className="project-name-form">
-          <label className="title-input-label">PROJECT TITLE</label>
-          <input className="title-input" type="text" onChange={(event) => setTitle(event.target.value)}/> 
-
+        {clickedProject.title && 
+          clickedProject.title
+        }
+      {!clickedProject.title &&
+          < label className="title-input-label">PROJECT TITLE</label>
+        }
+        {!clickedProject.title &&
+          <input className="title-input" type="text" onChange={(event) => setTitle(event.target.value)} />
+        }
         <button className="generate-palette-button" type="button" onClick={(event) => handleSubmit(event)}>generate palette</button>
       </form>
     </div>
   )
 }
-
-
 export default ProjectForm;
