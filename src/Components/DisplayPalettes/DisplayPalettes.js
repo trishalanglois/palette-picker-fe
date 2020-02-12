4897f478dd77fce1a99d83baacbd87a25805a0da
“Foster <fosterataylor@yahoo.com>
2020-02-12 15:12:02 -0700

    Add styling for DisplayPalettes

--- src/Components/DisplayPalettes/DisplayPalettes.js
+++ src/Components/DisplayPalettes/DisplayPalettes.js
@@ -6,29 +6,29 @@ import Color from '../Color/Color';
 export const DisplayPalettes = ({ color1,color2,color3,color4,color5,name }) => {
 
   return (
-    <aside className="palette-wrapper">
-          <div className="palettes-wrapper">
-              <h4 className="palette-title">{name}</h4>
+    <aside className="small-palette-wrapper">
+      <div className="palettes-wrapper">
+        <h4 className="palette-title">{name}</h4>
         <Color
-            key={color1} 
-            color={color1}
-            />
-        <Color
-            key={color2}
-            color={color2}
-            />
-        <Color
-            key={color3} 
-            color={color3}
-            />
-        <Color
-            key={color4} 
-            color={color4}
+          key={color1} 
+          color={color1}
         />
         <Color
-            key={color5} 
-            color={color5}
-         />
+          key={color2}
+          color={color2}
+        />
+        <Color
+          key={color3} 
+          color={color3}
+        />
+        <Color
+          key={color4} 
+          color={color4}
+        />
+        <Color
+          key={color5} 
+          color={color5}
+        />
       </div>
     </aside>
   )
--- src/reducers/palettes.js
+++ src/reducers/palettes.js
@@ -2,7 +2,7 @@ export const palettes = (state = [], action) => {
   console.log(action.palettes)
     switch (action.type) {
       case 'ADD_PALETTES': 
-        return [...state, action.palettes]
+        return [...state, ...action.palettes]
       default: 
         return state
     }
