ad944e71bb0f4f3c0fc04693c0d60c3fb7e5c3ed
Trisha Langlois <46874658+trishalanglois@users.noreply.github.com>
2020-02-11 17:01:27 -0700

    Connect ProjectForm to store to updated clickedProject

--- src/Components/ProjectForm/ProjectForm.js
+++ src/Components/ProjectForm/ProjectForm.js
@@ -2,33 +2,41 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './ProjectForm.scss';
 import { postProject } from '../../util/apiCalls';
-import { addProject } from '../../actions/actions';
+import { addProject, clickedProject } from '../../actions/actions';
+
 export const ProjectForm = ({ projectTitle, id }) => {
   const [title, setTitle] = useState("");
   const dispatch = useDispatch();
-  const clickedProject = useSelector(state => state.clickedProject)
+  const chosenProject = useSelector(state => state.clickedProject)
   
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const response = await postProject(title)
       dispatch(addProject(response.id[0], title))
+      dispatch(clickedProject(response.id[0], title))
     } catch (error) {
     }
   }
+  
+  useEffect(() => {
+
+  }, [title])
+  
   return (
     <div className="project-form-wrapper"> 
       <form className="project-name-form">
-        {clickedProject.title && 
-          clickedProject.title
+        {chosenProject.title && 
+          chosenProject.title
         }
-      {!clickedProject.title &&
+      {!chosenProject.title &&
           < label className="title-input-label">PROJECT TITLE</label>
         }
-        {!clickedProject.title &&
+        {!chosenProject.title &&
           <input className="title-input" type="text" onChange={(event) => setTitle(event.target.value)} />
         }
-        <button className="generate-palette-button" type="button" onClick={(event) => handleSubmit(event)}>generate palette</button>
+        {title &&  <button className="generate-palette-button" type="button" onClick={(event) => handleSubmit(event)}>name project</button>}
+
       </form>
     </div>
   )
